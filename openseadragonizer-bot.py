5bc3950d6db9658560b4d0501ed5e298fb91aaad
Antoine Vandecreme <ant.vand@gmail.com>
2016-05-27 15:52:02 -0400

    First commit.

---
+++ openseadragonizer-bot.py
@@ -0,0 +1,54 @@
+import praw
+import time
+import traceback
+import sqlite3
+from prawoauth2 import PrawOAuth2Mini
+from settings import app_key, app_secret, access_token, refresh_token, scopes
+
+db_conn = sqlite3.connect('sqlite.db')
+db_cursor = db_conn.cursor()
+
+db_cursor.execute("CREATE TABLE IF NOT EXISTS processed_submissions " + \
+                  "(id TEXT PRIMARY KEY)")
+db_conn.commit()
+
+subreddit = 'all'
+
+user_agent = "OpenSeadragonizer:v0.0.0 (by /u/openseadragonizer)"
+r = praw.Reddit(user_agent = user_agent)
+oauth_helper = PrawOAuth2Mini(r, app_key=app_key, app_secret=app_secret,
+    access_token=access_token, refresh_token=refresh_token, scopes=scopes)
+
+min_width = 2000
+min_height = 2000
+
+def get_message(url):
+    return "[Zoomable version of the image]" + \
+           "(https://openseadragon.github.io/openseadragonizer/?img=" + url + \
+           ")\n\n&nbsp;\n\n---\n" + \
+           "I'm a bot, please report any issue on " + \
+           "[GitHub](https://github.com/openseadragon/reddit-bot/issues)."
+
+while True:
+    try:
+        submissions = r.get_subreddit(subreddit).get_new(limit = 100)
+        for submission in submissions:
+            if hasattr(submission, 'preview') and \
+                submission.preview['images'] and \
+                db_cursor.execute("SELECT id FROM processed_submissions WHERE id=(?)",\
+                    (submission.id,)).fetchone() is None:
+                source = submission.preview['images'][0]['source']
+                if source['width'] > min_width or source['height'] > min_height:
+                    message = get_message(source['url'])
+                    submission.add_comment(message)
+                    db_cursor.execute("INSERT INTO processed_submissions(id) VALUES (?)",\
+                        (submission.id,))
+                    db_conn.commit()
+                    print("Posted comment in submission titled " +  submission.title)
+        time.sleep(30)
+    except praw.errors.OAuthInvalidToken:
+        # token expired, refresh 'em!
+        oauth_helper.refresh()
+    except Exception:
+        traceback.print_exc()
+        time.sleep(30)
